287e0a14c1ddf16300c1f2edb34f0e0ff97e8f88
Dibyadyuti Das <dibyadyutidas0@gmail.com>
2026-08-25 17:21:45 +0530

    [GitCode] Solved #1 Two Sum | Time: 0 ms (100%), Space: N/A

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,7 @@
-                }
             }
                     return new int[]{i, j};
         }
-                if (nums[i] + nums[j] == target) {
+                if (nums[i] + nums[j] == target) 
             for (int j = i + 1; j < n; j++) {
         return new int[]{};
     }
